3b6aa3413b79eb98744f050afc8740760ba424a1
Harshal Patel <patelharshal101@gmail.com>
2023-02-12 17:29:15 -0700

    Express config

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Helo");
+//Express dependancies 
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+const cors = require('cors');
+app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
