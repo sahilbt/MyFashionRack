ecfa3dde9052506a1a736985e7a27ea33d8e0a95
gabengu <gabengu@gmail.com>
2023-02-17 10:42:12 -0700

    scroll animation and social media icons

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -10,6 +10,6 @@ export default function App({ Component, pageProps }) {
   return(
     <main className = {font.className}>
         <Component {...pageProps} />
-    </main> 
+    </main>
   )
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,21 +1,27 @@
 import LandingNav from "../components/LandingNav"
-import zod from "../public/unknown-5 2.jpeg"
+import zod from "../public/unknown-52.jpeg"
 import Image from "next/image"
 
 export default function index(){
     return(
-        <div className="flex h-screen justify-end items-center">
-            <Image
-                className="-z-10 absolute opacity-30 top-0 h-screen w-auto object-left object-cover left-0 overflow-x-auto"
-                src = {zod}
-            />
-            <div className="mr-14 flex flex-col items-center">
-                <h1 className="text-5xl text-white text-center">Elevate Your Style</h1>
-                <p className="max-w-xs text-white text-lg text-center mt-6">
-                    Follow, share and view your favorite outfits from your favorite influencers and friends.
-                </p>
+        <div>
+            <div className="h-screen">
+                <Image
+                    className="-z-10 fixed opacity-30 h-screen w-auto object-left object-cover left-0"
+                    src = {zod}
+                />
+                <LandingNav />
+                <div className="mr-14 flex fixed -z-10 flex-col justify-center items-center h-screen right-64">
+                    <h1 className="text-5xl text-white text-center font-bold">Elevate Your Style</h1>
+                    <p className="max-w-xs text-white text-lg text-center mt-6">
+                        Follow, share and view your favorite outfits from your favorite influencers and friends.
+                    </p>
+                </div>
             </div>
-            <LandingNav />
+            <div className="h-screen bg-darkGrey">
+                
+            </div>
+                
         </div>
     )
 }
