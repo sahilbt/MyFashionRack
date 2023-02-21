624d033fd04e07a8364914f1799fd4cd27c8a09d
Sahil Bhatt <sahilb1017@gmail.com>
2023-02-20 20:44:35 -0700

    animation

--- frontend/components/LandingNav.js
+++ frontend/components/LandingNav.js
@@ -9,23 +9,33 @@ export default function LandingNav(){
     return(
         <div className = "fixed flex flex-col items-center w-64 bg-pink inset-y-0 right-0 text-white">
             <Image
-            className="h-60 w-auto mt-9"
+            className="h-52 w-auto mt-9"
                 src = {Logo}
             />
 
             <div className="absolute flex flex-col gap-y-4 justify-center items-center h-screen">
                 <div className="text-center">
-                    <a href="#" className="text-2xl">Login</a>
+                    <a href="#" className="text-2xl group">
+                        Login
+                        <span class="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                    </a>
                 </div>
                 <div className="text-center">
-                    <a href="#" className="text-2xl">Sign Up</a>
+                    <a href="#" className="text-2xl group">
+                        Sign Up
+                        <span class="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                    </a>
+
                 </div>
 
             </div>
 
-            <div className="flex-row absolute bottom-0">
+            <div className="flex absolute bottom-0">
                 <div className="text-center">
-                    <a href="#" className="text-xl">About</a>
+                    <a href="#" className="text-xl group">
+                        About
+                        <span class="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                    </a>
                 </div>
 
                 <div className="flex ml-6 mr-6 gap-6">
