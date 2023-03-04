e43100e8beb27ca5feaf66b5b1dcf3b6e7656b42
Sahil Bhatt <sahilb1017@gmail.com>
2023-03-04 15:36:42 -0700

    login page change + signup page added

--- frontend/pages/SignUp.js
+++ frontend/pages/SignUp.js
@@ -2,7 +2,7 @@ import Image from "next/Image"
 import Logo from "../public/LogoPink.png"
 import Link from "next/Link"
 
-export default function Login() {
+export default function SignUp() {
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen text-center bg-darkGrey">
             <div className="flex w-screen items-center content-center">
@@ -15,12 +15,13 @@ export default function Login() {
                             />
                         </Link>
                     </div>
-                    <h1 className="text-4xl text-white text-center pb-5">Login</h1>
+                    <h1 className="text-4xl text-white text-center pb-5">Sign Up</h1>
                     <div className="flex flex-col items-center gap-y-6">
                         <input type="email" name="email" placeholder="email" className="px-4 h-14 w-96 bg-lightGrey text-white rounded-3xl outline-white outline-2 focus:outline focus:outline-white hover:outline hover:outline-[#464646]"/>
                         <input type="password" name="password" placeholder="password" className="px-4 h-14 w-96 bg-lightGrey text-white rounded-3xl outline-white outline-2 focus:outline focus:outline-white hover:outline hover:outline-[#464646]"/>
+                        <input type="password" name="verify" placeholder="verify password" className="px-4 h-14 w-96 bg-lightGrey text-white rounded-3xl outline-white outline-2 focus:outline focus:outline-white hover:outline hover:outline-[#464646]"/>
                         <button className="bg-pink text-white rounded-3xl w-48 h-14 hover:bg-[#AA4E65]">
-                            Login
+                            Sign Up
                         </button>
                      <div className="relative flex items-center justify-center mt-4 border border-t w-96">
                         <div className="absolute px-5 bg-darkGrey text-white">OR</div>
@@ -29,9 +30,9 @@ export default function Login() {
                         Continue with Google
                     </button>
                     <div className="flex text-white">
-                        <div>Don't have an account?</div>
-                            <Link href="/SignUp" className="group text-pink pl-3">
-                                Sign Up Here!
+                        <div>Already have an account?</div>
+                            <Link href="/Login" className="group text-pink pl-3">
+                                Login Here!
                                 <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-pink"></span>
                             </Link>
                         </div>
