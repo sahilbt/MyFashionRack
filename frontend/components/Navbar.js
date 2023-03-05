d6f4532a988c8d27832ca5c5b8b70c7e3fbaccbe
Sahil Bhatt <sahilb1017@gmail.com>
2023-03-04 21:25:18 -0700

    fixed navbar and feedpage

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -8,55 +8,59 @@ import AccountButton from "../public/account-button.svg"
 export default function Navbar(params) {
     return(
         <div>
-            <div className='w-screen flex justify-between items-center h-24 px-44 text-white bg-pink'>
-                <div className="flex items-center space-x-6">
-                    <Link href="/Feed">
-                        <Image
-                            className="w-auto h-14"
-                            src = {Logo}
-                        />
-                    </Link>
-                    <input type="text" name="search" placeholder="" className="px-4 h-10 w-80 bg-[#DE839A] text-white rounded-3xl outline outline-white outline-2 focus:outline focus:outline-white "/>
-                </div>
-                
+            <div className='w-screen  h-24  text-white bg-pink'>
+                <div className="grid place-items-center">
+                    <div className="flex justify-between items-center h-24 w-[80%]">     
+                        <div className="flex items-center space-x-6">
+                            <Link href="/Feed">
+                                <Image
+                                    className="w-auto h-14"
+                                    src = {Logo}
+                                />
+                            </Link>
+                            <input type="text" name="search" placeholder="" className="px-4 h-10 w-80 bg-[#DE839A] text-white rounded-3xl outline outline-white outline-2 focus:outline focus:outline-white "/>
+                        </div>
+                        
 
-                <div className="flex space-x-14">
-                    <div className="grid place-items-center gap-y-1">
-                        <Link href="#">
-                            <Image
-                                className="w-auto h-10"
-                                src={AddButton}
-                            />
-                        </Link>
-                        <div className="text-xs">
-                            New Post
+                        <div className="flex space-x-14">
+                            <div className="grid place-items-center gap-y-1">
+                                <Link href="#">
+                                    <Image
+                                        className="w-auto h-10"
+                                        src={AddButton}
+                                    />
+                                </Link>
+                                <div className="text-xs">
+                                    New Post
+                                </div>
+                            </div>
+
+
+                            <div className="grid place-items-center gap-y-1">
+                                <Link href="#">
+                                    <Image
+                                        className="w-auto h-10"
+                                        src={LogOutButton}
+                                    />
+                                </Link>
+                                <div className="text-xs">
+                                    Logout
+                                </div>
+                            </div>
+
+                            <div className="grid place-items-center gap-y-1">
+                                <Link href="#">
+                                    <Image
+                                        className="w-auto h-10"
+                                        src={AccountButton}
+                                    />
+                                </Link>
+                                <div className="text-xs">
+                                    Account
+                                </div>
+                            </div>                 
                         </div>
                     </div>
-
-
-                    <div className="grid place-items-center gap-y-1">
-                        <Link href="#">
-                            <Image
-                                className="w-auto h-10"
-                                src={LogOutButton}
-                            />
-                        </Link>
-                        <div className="text-xs">
-                            Logout
-                        </div>
-                    </div>
-
-                    <div className="grid place-items-center gap-y-1">
-                        <Link href="#">
-                            <Image
-                                className="w-auto h-10"
-                                src={AccountButton}
-                            />
-                        </Link>
-                        <div className="text-xs">
-                            Account
-                        </div>
-                    </div>                 
                 </div>
             </div>
         </div>
--- frontend/pages/Feed.js
+++ frontend/pages/Feed.js
@@ -6,37 +6,40 @@ export default function Feed() {
         <div>
             <Navbar/>
 
-            <div className='w-screen flex justify-between items-center px-44 text-white mt-10 '>
-                
-                <Link href="#">
-                    Streetwear
-                </Link>
+            <div className='w-screen  h-9  text-white mt-10'>
+                <div className="grid place-items-center">
+                    <div className="flex justify-between items-center h-9 w-[80%] bg-lightGrey rounded-xl outline outline-1 outline-pink px-7">
+                        <Link href="#" className="group">
+                            Streetwear
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                        </Link>
+                        <Link href="#" className="group">
+                            Workwear
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                        </Link>
+                        <Link href="#" className="group">
+                            Minimalist
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                        </Link>
+                        <Link href="#" className="group">
+                            Formal
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                        </Link>
+                        <Link href="#" className="group">
+                            Athleisure
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                        </Link>
+                        <Link href="#" className="group">
+                            Vintage
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                        </Link>
+                        <Link href="#" className="group">
+                            Second-hand
+                            <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
+                        </Link>
 
-                <Link href="#" >
-                    Workwear
-                </Link>
-
-                <Link href="#">
-                    Minimalist
-                </Link>
-
-                <Link href="#">
-                    Athleisure
-                </Link>
-
-                <Link href="#">
-                    Formal
-                </Link>
-
-                <Link href="#">
-                    Vintage
-                </Link>
-
-                <Link href="#">
-                    Second-hand
-                </Link>
-                
-
+                    </div>
+                </div>
             </div>
         </div>
     )
