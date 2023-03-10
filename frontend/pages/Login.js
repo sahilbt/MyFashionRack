4906c5947e77e029f30c53266b57157094b08053
Sahil Bhatt <sahilb1017@gmail.com>
2023-03-09 22:28:44 -0700

    signup and login made smaller

--- frontend/pages/Login.js
+++ frontend/pages/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
         <div className="flex flex-col items-center justify-center w-full h-screen text-center bg-darkGrey">
             <div className="flex w-full items-center content-center">
                 <div className="w-full grid place-items-center">
-                    <div className="w-32 mb-6">
+                    <div className="w-28 mb-6">
                         <Link href="/">
                             <Image
                                 className="w-auto"
@@ -15,17 +15,17 @@ export default function Login() {
                             />
                         </Link>
                     </div>
-                    <h1 className="text-4xl text-white text-center pb-5">Login</h1>
+                    <h1 className="text-3xl text-white text-center pb-5">Login</h1>
                     <div className="flex flex-col items-center gap-y-6">
-                        <input type="email" name="email" placeholder="email" className="px-4 h-14 w-96 bg-lightGrey text-white rounded-3xl outline-white outline-2 focus:outline focus:outline-white hover:outline hover:outline-[#464646]"/>
-                        <input type="password" name="password" placeholder="password" className="px-4 h-14 w-96 bg-lightGrey text-white rounded-3xl outline-white outline-2 focus:outline focus:outline-white hover:outline hover:outline-[#464646]"/>
-                        <button className="bg-pink text-white rounded-3xl w-48 h-14 hover:bg-[#AA4E65]">
+                        <input type="email" name="email" placeholder="email" className="px-4 h-12 w-96 bg-lightGrey text-white rounded-3xl outline-white outline-2 focus:outline focus:outline-white hover:outline hover:outline-[#464646]"/>
+                        <input type="password" name="password" placeholder="password" className="px-4 h-12 w-96 bg-lightGrey text-white rounded-3xl outline-white outline-2 focus:outline focus:outline-white hover:outline hover:outline-[#464646]"/>
+                        <button className="bg-pink text-white rounded-3xl w-48 h-12 hover:bg-[#AA4E65]">
                             Login
                         </button>
                      <div className="relative flex items-center justify-center mt-4 border border-t w-96">
                         <div className="absolute px-5 bg-darkGrey text-white">OR</div>
                     </div>
-                    <button className="bg-darkGrey text-white rounded-3xl mt-4 w-64 h-14 outline outline-2 outline-white hover:outline-pink">
+                    <button className="bg-darkGrey text-white rounded-3xl mt-4 w-64 h-12 outline outline-2 outline-white hover:outline-pink">
                         Continue with Google
                     </button>
                     <div className="flex text-white">
