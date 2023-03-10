34cd70990c6d089dcba0ac473f0c991dd11f48cf
gabengu <gabengu@gmail.com>
2023-03-10 10:37:48 -0700

    Feed

--- frontend/pages/Feed.js
+++ frontend/pages/Feed.js
@@ -1,8 +1,4 @@
 import Link from "next/Link"
-<<<<<<< HEAD
-import { render } from "react-dom"
-=======
->>>>>>> 9039be8f95eaa4f62e8e7c2956538fd3e07e9933
 import Navbar from "../components/Navbar"
 import Post from "../components/Post"
 import posts from "../posts"
@@ -14,7 +10,6 @@ export default function Feed() {
         )
     })
     return(
-<<<<<<< HEAD
         <div className="w-full">
             <Navbar/>
             <div className='w-full h-9 text-white mt-10'>
@@ -79,56 +74,6 @@ export default function Feed() {
             <div className="h-screen">
 
             </div>
-=======
-        <div className="mt-32 flex justify-center">
-            <Navbar />
-            <div className="w-10/12">
-                <div className="text-white flex justify-between items-center h-9 bg-lightGrey rounded-xl outline outline-1 outline-pink px-24">
-                    <Link href="#" className="group">
-                        Streetwear
-                        <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
-                    </Link>
-                    <Link href="#" className="group">
-                        Workwear
-                        <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
-                    </Link>
-                    <Link href="#" className="group">
-                        Minimalist
-                        <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
-                    </Link>
-                    <Link href="#" className="group">
-                        Formal
-                        <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
-                    </Link>
-                    <Link href="#" className="group">
-                        Athleisure
-                        <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
-                    </Link>
-                    <Link href="#" className="group">
-                        Vintage
-                        <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
-                    </Link>
-                    <Link href="#" className="group">
-                        Second-hand
-                        <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-white"></span>
-                    </Link>
-                </div>
-
-                <div className="flex h-96 ">
-                    <div className="bg-lightGrey h-96 w-60 rounded-xl outline outline-1 outline-pink">
-                        
-                    </div>
-                
-                    <div className="bg-lightGrey h-60 w-60 rounded-xl outline outline-1 outline-pink">
-                    
-                    </div>
-                    
-                    <div className="bg-lightGrey h-60 w-60 rounded-xl outline outline-1 outline-pink">
-                    
-                    </div>
-                </div>
-            </div>
->>>>>>> 9039be8f95eaa4f62e8e7c2956538fd3e07e9933
         </div>
     )
 }
