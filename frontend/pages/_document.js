cc112193476eadb8a43375bc138f92c3ed879069
gabengu <gabengu@gmail.com>
2023-03-10 11:06:00 -0700

    Testing

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,13 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-<<<<<<< HEAD
       <Head />
       <body className="bg-darkGrey mr-64 w-screen max-w-full">
-=======
-        <Head/>
-      <body className="w-screen max-w-full bg-darkGrey mr-64">
->>>>>>> 9039be8f95eaa4f62e8e7c2956538fd3e07e9933
         <Main />
         <NextScript />
       </body>
