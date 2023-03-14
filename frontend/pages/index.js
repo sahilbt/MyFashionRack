709e44c8b1bbd99433bba4e1148bca00ec637aec
gabengu <gabengu@gmail.com>
2023-03-14 16:40:40 -0600

    Scroll Animations

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,6 +6,7 @@ import Illustration1 from "../public/undraw_loving_it_re_jfh4.svg"
 import Illustration2 from "../public/undraw_online_shopping_re_k1sv.svg"
 import Illustration3 from "../public/undraw_shopping_re_hdd9.svg"
 import Illustration4 from "../public/undraw_login_re_4vu2.svg"
+import { motion } from "framer-motion"
 
 export default function index(){
     return(
@@ -28,29 +29,77 @@ export default function index(){
                 </div>
             </div>
             <div className="py-40 bg-darkGrey mr-64">
-                <div className="flex justify-center items-center gap-3 mb-24">
-                    <Image src = {Illustration1} className="w-1/3 h-auto"/>
-                    <p className="text-white text-3xl max-w-md text-center">
-                        See a fit that you're feeling? Give it a like so you can return to it later!
-                    </p>
+                <div className="flex flex-row justify-center items-center gap-y-3 gap-x-20 mb-24 h-64">
+                    <motion.div className="w-1/3 h-auto"
+                        initial={{x:-100}}
+                        whileInView={{x:0}}
+                        transition={{duration:0.7}}
+                    >
+                        <Image src = {Illustration1}/>
+                    </motion.div>
+                    <motion.div className="text-white text-3xl max-w-md text-center min-h-full flex items-center justify-center"
+                        initial={{x:100}}
+                        whileInView={{x:0}}
+                        transition={{duration:0.7}}
+                    >
+                        <div>
+                            See a fit that you're feeling? Give it a like so you can return to it later!
+                        </div>
+                    </motion.div>
                 </div>
                 <div className="flex justify-center items-center gap-3 mb-24">
-                    <p className="text-white text-3xl max-w-md text-center">
-                        Only like specific pieces of the outfit? Don't worry, there are links to each individual article of clothing!
-                    </p>
-                    <Image src = {Illustration2} className="w-1/3 h-auto"/>
+                    <motion.div className="text-white text-3xl max-w-md text-center min-h-full flex items-center justify-center"
+                        initial={{x:-100}}
+                        whileInView={{x:0}}
+                        transition={{duration:0.7}}
+                    >
+                        <div>
+                            See a fit Only like specific pieces of the outfit? Don't worry, there are links to each individual article of clothing!
+                        </div>
+                    </motion.div>
+                    <motion.div className="w-1/3 h-auto"
+                        initial={{x:100}}
+                        whileInView={{x:0}}
+                        transition={{duration:0.7}}
+                    >
+                        <Image src = {Illustration2}/>
+                    </motion.div>
                 </div>
                 <div className="flex justify-center items-center gap-3 mb-24">
-                    <Image src = {Illustration3} className="w-1/3 h-auto"/>
-                    <p className="text-white text-3xl max-w-md text-center">
-                        Only like certain styles? You can filter your feed to only show the selected style!
-                    </p>
+                    <motion.div className="w-1/3 h-auto"
+                        initial={{x:-100}}
+                        whileInView={{x:0}}
+                        transition={{duration:0.7}}
+                    >
+                        <Image src = {Illustration3}/>
+                    </motion.div>
+                    <motion.div className="text-white text-3xl max-w-md text-center min-h-full flex items-center justify-center"
+                        initial={{x:100}}
+                        whileInView={{x:0}}
+                        transition={{duration:0.7}}
+                    >
+                        <div>
+                            Only like certain styles? You can filter your feed to only show the selected style!
+                        </div>
+                    </motion.div>
                 </div>
                 <div className="flex justify-center items-center gap-3">
-                    <p className="text-white text-3xl max-w-md text-center">
-                        Ready to find new inspiration for your style? Create an account or log in today!
-                    </p>
-                    <Image src = {Illustration4} className="w-1/3 h-auto"/>
+                <motion.div className="text-white text-3xl max-w-md text-center min-h-full flex items-center justify-center"
+                        initial={{x:-100}}
+                        whileInView={{x:0}}
+                        transition={{duration:0.7}}
+                    >
+                        <div>
+                            Ready to find new inspiration for your style? Create an account or log in today!
+                        </div>
+                    </motion.div>
+                    <motion.div className="w-1/3 h-auto"
+                        initial={{x:100}}
+                        whileInView={{x:0}}
+                        transition={{duration:0.7}}
+                    >
+                        <Image src = {Illustration4}/>
+                    </motion.div>
                 </div>
             </div>
         </div>
