ce075f2a11dd1b009fae26241a29628521051f35
Sahil Bhatt <sahilb1017@gmail.com>
2023-03-15 15:35:50 -0600

    add picture to model with background

--- frontend/components/Modal.js
+++ frontend/components/Modal.js
@@ -1,15 +1,41 @@
-import { motion } from "framer-motion";
-import Backdrop from "./Backdrop";
+import { motion } from "framer-motion"
+import Backdrop from "./Backdrop"
+import Image from "next/Image"
 
 export default function Modal({data, handleClick}){
     return(
         <Backdrop handleClick={handleClick}>
-            <motion.div
-                onClick={(event) => event.stopPropagation()}
-                className="bg-lightGrey w-1/2 h-1/2"
-            >
-            <h1>{data.Username}</h1>
+            <motion.div onClick={(event) => event.stopPropagation()} className="bg-lightGrey w-3/5 h-2/3 relative grid grid-cols-2 ">
+
+                <div className="relative bg-black">
+                    <Image alt="Outfit" className="object-contain" src={data.Image} fill />
+                </div>
+
+
+                    {/* <div className="w-1/2">
+                        fedfdf
+                    </div> */}
+
+                <div className="flex-col ">
+                    <div>
+                        {data.Username}
+                    </div>
+                    <div>
+                        {data.Description}
+                    </div>
+
+                    <div>
+                        {data.Date}
+                    </div>
+
+                    <div>
+                        {data.Likes}
+                    </div>
+                </div>
+
+
             </motion.div>
+            
         </Backdrop>
     )
 }
--- frontend/components/Post.js
+++ frontend/components/Post.js
@@ -10,15 +10,15 @@ export default function Post(props){
         setModal(() => !modal)
     }
     return(
-        <div>
+        <div className="flex flex-col">
             <AnimatePresence>
                 {modal && <Modal data={props} modal={modal} handleClick={handleClick}/>}
             </AnimatePresence>
-            <div className="bg-lightGrey p-2 rounded-t-md">
+            <div className="bg-lightGrey p-2 rounded-t-xl">
                 <h1>{props.Username}</h1>
             </div>
-            <Image onClick={handleClick} alt="Outift" className="h-auto w-auto" src={props.Image} width = {480} height = {200}/>
-            <div className="bg-lightGrey p-2 rounded-b-md flex justify-between">
+            <Image onClick={handleClick} alt="Outfit" src={props.Image} width = {480} height = {200}/>
+            <div className="bg-lightGrey p-2 rounded-b-xl flex justify-between">
                 <h1>{props.Description}</h1>
                 <h1>{props.Likes}</h1>
             </div>
