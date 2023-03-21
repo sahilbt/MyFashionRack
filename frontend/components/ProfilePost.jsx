63bd3ff67e2f0d237ac07b6003aa35d6d442f776
gabengu <gabengu@gmail.com>
2023-03-20 22:55:48 -0600

    UserProfile mostly done

--- frontend/components/ProfilePost.jsx
+++ frontend/components/ProfilePost.jsx
@@ -3,13 +3,20 @@ import Modal from "./Modal.jsx"
 import { useState } from "react"
 import { AnimatePresence } from "framer-motion"
 
-export default function ProfilePost(){
+export default function ProfilePost(props){
     const [modal, setModal] = useState(false)
     console.log(modal)
     function handleClick(){
         setModal(() => !modal)
     }
     return(
-        <div></div>
+        <div onClick={handleClick} className="w-full aspect-square">
+            <div className="bg-black relative -z-10 w-full h-full">
+                <Image alt="Outfit" className="object-cover rounded-lg" src={props.Image} fill/>
+            </div>
+            <AnimatePresence>
+                {modal && <Modal data={props} modal={modal} handleClick={handleClick}/>}
+            </AnimatePresence>
+        </div>
     )
 }
