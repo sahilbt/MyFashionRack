a3284ac163d80b9d85d82f5b918efd9756ccfb31
Sahil Bhatt <sahilb1017@gmail.com>
2023-03-22 21:51:42 -0600

    add file upload to create post

--- frontend/pages/create.jsx
+++ frontend/pages/create.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Navbar from '../components/Navbar'
 import { useAppContext } from "../context/userContext";
+import addButton from "../public/addPhoto.svg"
+import Image from "next/image"
 
 export default function create(params) {
     const {user} = useAppContext()
@@ -23,6 +25,12 @@ export default function create(params) {
             )
         })
     }
+
+    const [file, setFile] = useState();
+    function handleChange(e) {
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
+
     return(
         <div className="w-full h-screen">
             <Navbar/>
@@ -31,11 +39,20 @@ export default function create(params) {
                     Create Post
                 </div>
                 <div className="relative flex items-center justify-center mt-6 border border-t w-[65%]"></div>
-                <div className="flex w-[65%] gap-10 justify-center my-10">
-                    <div className="w-1/2 h-[500px] bg-lightGrey rounded-lg border-dashed border-2 border-pink">
 
+                <div className="flex w-[65%] gap-10 justify-center my-10">
+                    <div className="relative flex items-center justify-center w-1/2 h-[450px] bg-lightGrey rounded-lg border-dashed border-2 border-pink  hover:bg-[#515151]">
+                        <label htmlFor="drop" className="w-full h-full flex flex-col justify-center" >
+                            <div className="flex flex-col items-center justify-center">
+                                <Image alt="" src = {addButton}/>
+                                <p className="text-sm">Click to upload OR drag and drop</p>
+                            </div>
+                            <input id="drop" type="file" className="hidden" onChange={handleChange} />
+                            <Image alt="" className="object-contain" src={file} fill />
+                            
+                        </label>
                     </div>
-                    <div className="w-1/2 h-[500px] flex flex-col gap-3">
+                    <div className="w-1/2 h-[450px] flex flex-col gap-3">
                         <div className="h-1/3">
                             <h1 className="text-2xl">
                                 Add a description.
@@ -63,7 +80,7 @@ export default function create(params) {
                     </div>
                 </div>
                 <div className="w-[65%] flex">
-                    <button className="bg-pink rounded-full px-2 py-1 ml-auto">
+                    <button className="bg-pink rounded-full px-2 py-1 ml-auto hover:bg-[#AA4E65]">
                         Create Post
                     </button>
                 </div>
