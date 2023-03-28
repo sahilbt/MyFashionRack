588276b236d862988edaeff174d146c3259b9f07
gabengu <gabengu@gmail.com>
2023-03-28 17:13:25 -0600

    Search Functionality

--- frontend/components/SearchBar.jsx
+++ frontend/components/SearchBar.jsx
@@ -3,32 +3,54 @@ import { motion } from "framer-motion"
 import X from "../public/xmark-solid.svg"
 import Image from "next/Image"
 
-export default function SearchBar({setSearchBar}){
-    const[search, setSearch] = useState("")
-    function submitHandler(e){
+export default function SearchBar({ setSearchBar }) {
+    const [search, setSearch] = useState("")
+    const [results, setResults] = useState([""])
+    function submitHandler(e) {
         e.preventDefault()
-        console.log(search)
+        setResults([])
+        if(search.length > 0){
+            usernames.map(index => {
+                if (index.includes(search)) {
+                    setResults(prev => {
+                        return (
+                            [...prev, index]
+                        )
+                    })
+                }
+            })
+        }
     }
-    return(
-        <motion.div 
-            className="fixed h-full w-[27%] bg-darkGrey border-r-4 border-lightGrey z-10 p-7 flex flex-col items-center "
-            initial={{x:"-100vw"}}
-            animate={{x:0}}
-            exit={{x:"-100vw"}}
-            transition={{duration: 0.4}}
+    const renderResults = 
+    <div>
+        {(results.length > 0) ? results.map(result => {
+            return (
+                <a href="#" className="block text-2xl">{result}</a>
+            )
+        }): <div className="text-2xl">No Results Found</div>}
+    </div>
+
+    const usernames = ["pp", "new", "lilbaby", "pppoopoo"]
+    return (
+        <motion.div
+            className="fixed h-full w-[27%] bg-darkGrey border-r-4 border-lightGrey z-10 p-7"
+            initial={{ x: "-100vw" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100vw" }}
+            transition={{ duration: 0.4 }}
         >
             <form onSubmit={submitHandler} className="w-full">
                 <div className="flex justify-between mb-7 items-center">
                     <div className="text-white text-4xl">Search</div>
-                    <Image src={X} className="w-auto h-11 cursor-pointer hover:scale-[1.2] duration-75" onClick={() => {setSearchBar(prev => !prev)}}/>
+                    <Image src={X} className="w-auto h-11 cursor-pointer hover:scale-[1.2] duration-75" onClick={() => { setSearchBar(prev => !prev) }} />
                 </div>
-                <input value={search} onChange={(event) => {setSearch(event.target.value)}} placeholder="Search for a user"
+                <input value={search} onChange={(event) => { setSearch(event.target.value) }} placeholder="Search for a user"
                     className="px-4 h-10 w-full bg-lightGrey border-2 border-white text-white rounded-3xl"
                 />
             </form>
-            <div className="relative flex items-center justify-center mt-10 border-2 border-[#4F4F4F] w-full rounded-full"></div>
+            <div className="relative flex items-center justify-center mt-10 mb-5 border-2 border-[#4F4F4F] w-full rounded-full" />
             <div>
-
+                {renderResults}
             </div>
         </motion.div>
     )
