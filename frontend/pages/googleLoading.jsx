e3cde53a6c9a888e9d83c07db8d8a6bbafef9809
Harshal Patel <patelharshal101@gmail.com>
2023-03-29 14:39:14 -0600

    google oauth and other routes

--- frontend/pages/googleLoading.jsx
+++ frontend/pages/googleLoading.jsx
@@ -3,17 +3,33 @@ import { useEffect,useState  } from "react";
 import { useRouter } from "next/router";
 import Axios from "axios";
 Axios.defaults.withCredentials = true;
+import { useAppContext } from "../context/userContext";
 
 export default function googleLoading (){
+    const { user, setUser } = useAppContext();
     const router = useRouter();
     useEffect(() => {
-        Axios.get("http://localhost:8000/authentication/google/googleCheck")
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function(err){
-            console.log(err)
-        })
+        const  google = async() => {
+            await Axios.get("http://localhost:8000/authentication/google/getGoogle")
+            .then(function (response) {
+                console.log(response.data)
+                if(response.data.displayName){
+                    setUser(response.data);
+                    router.push('/users/me');
+                    
+                }else{
+                    setUser(response.data);
+                    router.push('/RegisterDetails');
+                    
+                }
+            })
+            .catch(function(err){
+                console.log(err)
+            })
+        }
+
+        google();
+        
     },
     [])
     
