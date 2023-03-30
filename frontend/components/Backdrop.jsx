39c4431afdba31e50b7b61a33462e5ea7daa8ff8
Sahil Bhatt <sahilb1017@gmail.com>
2023-03-29 23:14:53 -0600

    modal updates

--- frontend/components/Backdrop.jsx
+++ frontend/components/Backdrop.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 export default function Backdrop({children, handleClick}){
     return(
         <motion.div onClick={handleClick}
-            className="fixed top-0 left-0 bg-[#111111e1] w-screen h-screen flex justify-center items-center"
+            className="fixed top-0 left-0 bg-[#111111e1] w-screen h-screen flex justify-center items-center z-10"
             initial={{opacity:0}}
             animate={{opacity:1}}
             exit={{opacity:0}}
